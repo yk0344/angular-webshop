f141c82cbfcbb4ccb53cdda9f0a7a654c309eaca
yk0344 <rv662549@edu.rocrivor.nl>
2022-06-10 14:43:36 +0200

    1 file modified, 4 added

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../products';
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
